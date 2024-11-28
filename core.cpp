6ff88bb160ad4e0e278836f9194dcf1a493363f7
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2024-11-28 17:07:01 +0330

    create reserve need fix

--- core.cpp
+++ core.cpp
@@ -7,6 +7,7 @@
 using json = nlohmann::json;
 using namespace std;
 
+// load--------------------------------------------
 json loadJsonFromFile(const string& filename) {
     ifstream file(filename);
     if (!file.is_open()) {
@@ -26,6 +27,7 @@ json loadJsonFromFile(const string& filename) {
     return data;
 }
 
+// struct--------------------------------------------
 struct User {
     string username;
     string password;
@@ -33,6 +35,25 @@ struct User {
     int balance;
 };
 
+struct Meal {
+    int id;
+    string day;
+    string name;
+    int price;
+    string meal_type;
+    string location;
+};
+
+struct Reservation {
+    int id;
+    string username;
+    string name;
+    string meal_type;
+    string location;
+    string day;
+};
+
+// parse--------------------------------------------
 vector<User> parseUsers(const json& data) {
     vector<User> users;
 
@@ -50,6 +71,43 @@ vector<User> parseUsers(const json& data) {
     return users;
 }
 
+vector<Meal> parseMeals(const json& data) {
+    vector<Meal> meals;
+
+    for (const auto& item : data) {
+        Meal meal;
+        meal.id = item.at("id");
+        meal.day = item.at("day");
+        meal.name = item.at("name");
+        meal.price = item.at("price");
+        meal.meal_type = item.at("meal_type");
+        meal.location = item.at("location");
+
+        meals.push_back(meal);
+    }
+
+    return meals;
+}
+
+vector<Reservation> parseReservations(const json& data) {
+    vector<Reservation> reservations;
+
+    for (const auto& item : data) {
+        Reservation reservation;
+        reservation.id = item.at("id");
+        reservation.username = item.at("username");
+        reservation.name = item.at("name");
+        reservation.meal_type = item.at("meal_type");
+        reservation.location = item.at("location");
+        reservation.day = item.at("day");
+
+        reservations.push_back(reservation);
+    }
+
+    return reservations;
+}
+
+// login--------------------------------------------
 User login(const vector<User>& users) {
     string username, password;
     cout << "Enter username: ";
@@ -68,6 +126,7 @@ User login(const vector<User>& users) {
     return {};
 }
 
+// save from json--------------------------------------------
 void saveUsersToFile(const vector<User>& users, const string& filename) {
     json jsonData;
 
@@ -96,6 +155,59 @@ void saveUsersToFile(const vector<User>& users, const string& filename) {
     }
 }
 
+void saveMealsToFile(const vector<Meal>& meals, const string& filename) {
+    json jsonData;
+
+    for (const auto& meal : meals) {
+        json mealJson = {
+            {"id", meal.id},
+            {"day", meal.day},
+            {"name", meal.name},
+            {"price", meal.price},
+            {"meal_type", meal.meal_type},
+            {"location", meal.location}
+        };
+
+        jsonData.push_back(mealJson);
+    }
+
+    ofstream file(filename);
+    if (file.is_open()) {
+        file << jsonData.dump(4);
+        file.close();
+        cout << "Meals saved to file successfully!" << endl;
+    } else {
+        cerr << "Error: Could not open file " << filename << endl;
+    }
+}
+
+void saveReservesToFile(const vector<Reservation>& reservations, const string& filename) {
+    json jsonData;
+
+    for (const auto& reservation : reservations) {
+        json reservationJson = {
+            {"id", reservation.id},
+            {"username", reservation.username},
+            {"name", reservation.name},
+            {"meal_type", reservation.meal_type},
+            {"location", reservation.location},
+            {"day", reservation.day}
+        };
+
+        jsonData.push_back(reservationJson);
+    }
+
+    ofstream file(filename);
+    if (file.is_open()) {
+        file << jsonData.dump(4);
+        file.close();
+        cout << "Reservations saved to file successfully!" << endl;
+    } else {
+        cerr << "Error: Could not open file " << filename << endl;
+    }
+}
+
+// create--------------------------------------------
 void createStudent(vector<User>& users, const string& filename) {
     User newUser;
 
@@ -114,6 +226,31 @@ void createStudent(vector<User>& users, const string& filename) {
     cout << "Student created successfully!" << endl;
 }
 
+void createReservation(vector<Reservation>& reservations, const string& filename) {
+    Reservation newReservation;
+    int maxID;
+
+    for (const auto& reservation : reservations) {
+        if (reservation.id > maxID) {
+            maxID = reservation.id;
+        }
+    }
+
+    newReservation.id = maxID + 1;
+    newReservation.username = "student"; // me ro biar
+    newReservation.name = "student"; // meal ro biar ba ID yeji bashe
+    newReservation.meal_type = "student"; // meal ro biar ba ID yeji bashe
+    newReservation.location = "student"; // meal ro biar ba ID yeji bashe
+    newReservation.day = "089"; // meal ro biar ba ID yeji bashe
+
+    reservations.push_back(newReservation);
+
+    saveReservesToFile(reservations, filename);
+
+    cout << "Reserved successfully!" << endl;
+}
+
+// delete from json--------------------------------------------
 void deleteUserFromJSON(const string& username, vector<User>& users, const string& filename) {
     auto it = std::remove_if(users.begin(), users.end(),
                              [&username](const User& user) {
@@ -147,6 +284,7 @@ void deleteUserFromJSON(const string& username, vector<User>& users, const strin
     }
 }
 
+// asset--------------------------------------------
 void deposit(User& me, vector<User>& users, int amount, const string& filename) {
     me.balance += amount;
 
@@ -162,8 +300,10 @@ void deposit(User& me, vector<User>& users, int amount, const string& filename) 
     cout << "Balance updated successfully! New balance: " << me.balance << endl;
 }
 
-void STmenu(User& me, vector<User>& users, const string& filename) {
-    int choice, amount;
+// menu--------------------------------------------
+void STmenu(User& me, vector<User>& users, const string& filename, vector<Meal>& meals) {
+    int choice, amount, meal_id;
+    string m, loc, d;
 
     cout << "what do you want?" << endl;
     cout << "1. reserve" << endl;
@@ -174,7 +314,25 @@ void STmenu(User& me, vector<User>& users, const string& filename) {
 
     switch (choice) {
         case 1:
-            cout << "Enter username to delete: ";
+            cout << "witch day?: ";
+            cin >> d;
+            cout << "witch meal?: ";
+            cin >> m;
+            cout << "self or buffet?: ";
+            cin >> loc;
+            for (auto& meal : meals) {
+                if (meal.day == d && meal.location == loc && meal.meal_type == m) {
+                    cout << meal.name << " - price: " << meal.price  << " - id: " << meal.id << endl;
+                }
+            }
+            cout << "witch one?(id): ";
+            cin >> meal_id;
+            for (auto& meal : meals) {
+                if (meal.id == meal_id) {
+                    cout << "jsfhshfshfkhsfh";
+                }
+            }
+
         break;
         case 2:
             cout << "Enter amount for deposit: ";
@@ -182,10 +340,10 @@ void STmenu(User& me, vector<User>& users, const string& filename) {
             deposit(me, users, amount, filename);
         break;
         case 3:
-            cout << "delete student reserve" << endl;
+            cout << "List Reservation" << endl;
         break;
         case 4:
-            cout << "delete student reserve" << endl;
+            cout << "delete delete Reservation" << endl;
         break;
     }
 }
@@ -214,16 +372,26 @@ void ADmenu(vector<User>& users, const string& filename) {
     }
 }
 
+// main--------------------------------------------
 int main() {
     string filename = "users.json";
+    string mealFile = "meals.json";
+    string reserveFile = "reservation.json";
+
     json data = loadJsonFromFile(filename);
+    json mealData = loadJsonFromFile(mealFile);
+    json reserveData = loadJsonFromFile(reserveFile);
+
     vector<User> users = parseUsers(data);
+    vector<Meal> meals = parseMeals(mealData);
+    vector<Reservation> reservations = parseReservations(reserveData);
+
     if (!users.empty()) {
         cout << "Data Loaded" << endl;
     }
     User me = login(users);
     if (me.role == "student") {
-        STmenu(me, users, filename);
+        STmenu(me, users, filename, meals);
     }
     else if (me.role == "admin") {
         ADmenu(users, filename);
